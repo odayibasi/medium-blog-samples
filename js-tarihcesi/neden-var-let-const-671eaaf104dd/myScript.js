9cef3ea73bbf4cbd64280e808dfa563bda8a6684
onurdayibasi <odayibasi@gmail.com>
2020-11-08 16:27:29 +0300

    let var const

--- js-tarihcesi/neden-var-let-const-671eaaf104dd/myScript.js
+++ js-tarihcesi/neden-var-let-const-671eaaf104dd/myScript.js
@@ -1,1 +1,37 @@
+const example0001 = (function () {
+    console.log("Example 1------");
+    var name = "Onur";
+    var height = 180;
+    var age;
+})();
+
+
+const example0002 = (function () {
+    name = "Onur";
+    height = 180;
+})();
+
+
+const example0003 = (function () {
+    console.log("Example 3------");
+    function sayHello(){
+        foo="World";
+    }
+    sayHello();
+    console.log(`Hello ${foo}`);
+})();
+
+
+
+const example0004 = (function () {
+    console.log("Example 4------");
+    function sayHello(){
+        var goo="World";
+    }
+    sayHello();
+    console.log(`Hello ${goo}`);
+})();
+
+
+
 
