8c139495ee9dfb51e2e72664e335e67c468616fa
onurdayibasi <odayibasi@gmail.com>
2020-12-08 23:36:25 +0300

    App Extra Test added

--- react-ui-mechanics/react-setup/src/App.test.js
+++ react-ui-mechanics/react-setup/src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, {sum} from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
+test('sum', () => {
+    expect(sum(3, 4)).toEqual(7);
+});
+
+
