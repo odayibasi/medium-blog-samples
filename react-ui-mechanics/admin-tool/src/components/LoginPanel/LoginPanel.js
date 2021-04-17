99408c1c1325924cef162fc895a7662168926a2b
onurdayibasi <odayibasi@gmail.com>
2021-04-17 17:14:35 +0300

    State added

--- react-ui-mechanics/admin-tool/src/components/LoginPanel/LoginPanel.js
+++ react-ui-mechanics/admin-tool/src/components/LoginPanel/LoginPanel.js
@@ -6,15 +6,31 @@ import './LoginPanel.scss';
 
 export class LoginPanel extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleLogin = () => {
+        const {username, password} = this.state;
+        alert(`username:${username} and password: ${password}`)
+        this.props.handlePageChange(routes.admin.path)
+    }
+
     render() {
 
         return (
             <div>
                 <form className='login-panel-container' noValidate autoComplete="on">
-                    <TextField required label="Username"/>
-                    <TextField required label="Password" type="password" autoComplete="current-password"/>
+                    <TextField required label="Username"
+                               onChange={(e) => this.setState({username: e.target.value})}/>
+                    <TextField required label="Password" type="password" autoComplete="current-password"
+                               onChange={(e) => this.setState({password: e.target.value})}/>
                     <Button
-                        onClick={(e) => this.props.handlePageChange(routes.admin.path)}
+                        onClick={(e) => this.handleLogin()}
                         variant="contained" color="primary">Login</Button>
                 </form>
 
