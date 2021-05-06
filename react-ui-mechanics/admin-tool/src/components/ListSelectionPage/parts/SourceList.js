b620e29f1b3c8cbc77a03841e3d5206183d88faf
onurdayibasi <odayibasi@gmail.com>
2021-05-06 12:09:07 +0300

    ListSelectionPage filtering handlings added

--- react-ui-mechanics/admin-tool/src/components/ListSelectionPage/parts/SourceList.js
+++ react-ui-mechanics/admin-tool/src/components/ListSelectionPage/parts/SourceList.js
@@ -7,38 +7,57 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import CommentIcon from '@material-ui/icons/Comment';
+import {TextField} from "@material-ui/core";
 
 
 export default class SourceList extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			filterText: '',
+		}
+	}
+
+	handleChange = (e) => {
+		this.setState({filterText: e.target.value})
+	}
+
 
 	render() {
 		const {sourceList, sourceSelects, handleToggleSelection} = this.props;
+		const {filterText} = this.state;
+
+		const filteredSourceList = sourceList.filter(el => `Line item ${el}`.includes(filterText))
 
 		return (
-			<List className="source-list">
-				{sourceList.map((value) => {
-					const labelId = `checkbox-list-label-${value}`;
+			<div class="source-list">
+				<TextField id="standard-search" label="Search field" type="search" onChange={this.handleChange}/>
+				<List>
+					{filteredSourceList.map((value) => {
+						const labelId = `checkbox-list-label-${value}`;
 
-					return (
-						<ListItem key={value} dense button onClick={(e)=>{
-							console.log('clicked');
-							handleToggleSelection(value)
-						}}>
-							<ListItemIcon>
-								<Checkbox
-									edge="start"
-									checked={sourceSelects.indexOf(value) !== -1}
-									tabIndex={-1}
-									disableRipple
-									inputProps={{'aria-labelledby': labelId}}
-								/>
-							</ListItemIcon>
-							<ListItemText id={labelId} primary={`Line item ${value}`}/>
-						</ListItem>
-					);
-				})}
-			</List>
+						return (
+							<ListItem key={value} dense button onClick={(e) => {
+								console.log('clicked');
+								handleToggleSelection(value)
+							}}>
+								<ListItemIcon>
+									<Checkbox
+										edge="start"
+										checked={sourceSelects.indexOf(value) !== -1}
+										tabIndex={-1}
+										disableRipple
+										inputProps={{'aria-labelledby': labelId}}
+									/>
+								</ListItemIcon>
+								<ListItemText id={labelId} primary={`Line item ${value}`}/>
+							</ListItem>
+						);
+					})}
+				</List>
+			</div>
+
 		);
 
 	}
