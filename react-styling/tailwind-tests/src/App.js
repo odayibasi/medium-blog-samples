6848df593bacaaa141c23f5cf8614ab3484dbbc6
onurdayibasi <odayibasi@gmail.com>
2021-09-21 21:58:14 +0300

    Tailwind css test

--- react-styling/tailwind-tests/src/App.js
+++ react-styling/tailwind-tests/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div className="flex-shrink-0">
+          <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo"/>
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-gray-500">You have a new message!</p>
+        </div>
+      </div>
+
+
+      
+      
+      <div
+        className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0" src="/img/logo192.png"
+             alt="Woman's Face"/>
+        <div className="text-center space-y-2 sm:text-left">
+          <div className="space-y-0.5">
+            <p className="text-lg text-black font-semibold">
+              Erin Lindford
+            </p>
+            <p className="text-gray-500 font-medium">
+              Product Engineer
+            </p>
+          </div>
+          <button
+            className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message
+          </button>
+        </div>
+      </div>
+
+
+      <button className="btn btn-green">
+        Button
+      </button>
+
     </div>
   );
 }
--- react-styling/tailwind-tests/tailwind.config.js
+++ react-styling/tailwind-tests/tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		screens: {
+			'tablet': '640px',
+			// => @media (min-width: 640px) { ... }
+
+			'laptop': '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			'desktop': '1280px',
+			// => @media (min-width: 1280px) { ... }
+		}
 	},
 	variants: {
 		extend: {},
