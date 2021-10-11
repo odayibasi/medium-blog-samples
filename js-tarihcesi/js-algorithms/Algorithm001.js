0857d4cda63f08df93d22be066641bb463cda929
onurdayibasi <odayibasi@gmail.com>
2021-10-11 19:47:26 +0300

    Algorithm added

--- js-tarihcesi/js-algorithms/Algorithm001.js
+++ js-tarihcesi/js-algorithms/Algorithm001.js
@@ -1,8 +1,10 @@
 let dayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
 function dayOfX(dayName,X){
+   console.log(dayName, X);
    const index=dayNames.findIndex(el=>el==dayName);
    const newIndex= (index+X)%7;
+   console.log(index, newIndex);
    return dayNames[newIndex];
 }
 
