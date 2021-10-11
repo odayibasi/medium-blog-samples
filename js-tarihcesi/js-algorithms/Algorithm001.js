98ba26d2267803d0d94ccabcd82a099013d693db
onurdayibasi <odayibasi@gmail.com>
2021-10-11 20:19:55 +0300

    Algorithm added

--- js-tarihcesi/js-algorithms/Algorithm001.js
+++ js-tarihcesi/js-algorithms/Algorithm001.js
@@ -1,13 +1,16 @@
-let dayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+let dayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+dayNames.forEach((el, index) =>
+  console.log(`Index:${index} ${dayNames[index]}`)
+);
 
 function dayOfX(dayName, X) {
    console.log('dayOfX func called', dayName, X);
-   const index = dayNames.findIndex(el => el == dayName);
+   const index = dayNames.findIndex((el) => el == dayName);
    const newIndex = (index + X) % 7;
    console.log('index:' + index + ' newIndex:' + newIndex);
    return dayNames[newIndex];
 }
-
 
 console.log('Day Of X Wed', dayOfX('Wed', 2));
 console.log('Day Of X Sat', dayOfX('Sat', 5));
