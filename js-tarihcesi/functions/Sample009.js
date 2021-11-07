a756375157367aac184669265db12135dd3eb154
onurdayibasi <odayibasi@gmail.com>
2021-11-07 10:01:46 +0300

    Sample10 added

---
+++ js-tarihcesi/functions/Sample009.js
@@ -0,0 +1,68 @@
+const withConstructor = (constructor) => (o) => ({
+  __proto__: { constructor },
+  ...o,
+});
+
+const pipe =
+  (...fns) =>
+  (x) =>
+    fns.reduce((y, f) => f(y), x);
+// or `import pipe from 'lodash/fp/flow';`
+// Set up some functional mixins
+
+const trace = (label) => (value) => {
+  console.log(`${label}: ${value}`);
+  return value;
+};
+
+const withFlying = (o) => {
+  let isFlying = false;
+  return {
+    ...o,
+    fly() {
+      isFlying = true;
+      return this;
+    },
+    land() {
+      isFlying = false;
+      return this;
+    },
+    isFlying: () => isFlying,
+  };
+};
+const withBattery =
+  ({ capacity }) =>
+  (o) => {
+    let percentCharged = 100;
+    return {
+      ...o,
+      draw(percent) {
+        const remaining = percentCharged - percent;
+        percentCharged = remaining > 0 ? remaining : 0;
+        return this;
+      },
+      getCharge: () => percentCharged,
+      getCapacity: () => capacity,
+    };
+  };
+const createDrone = ({ capacity = '3000mAh' }) =>
+  pipe(
+    withFlying,
+    trace('withFlying'),
+    withBattery({ capacity }),
+    trace('withBattery'),
+    withConstructor(createDrone),
+    trace('withConstructor')
+  )({});
+
+const myDrone = createDrone({ capacity: '5500mAh' });
+console.log(`
+  can fly:  ${myDrone.fly().isFlying() === true}
+  can land: ${myDrone.land().isFlying() === false}
+  battery capacity: ${myDrone.getCapacity()}
+  battery status: ${myDrone.draw(50).getCharge()}%
+  battery drained: ${myDrone.draw(75).getCharge()}% remaining
+`);
+console.log(`
+  constructor linked: ${myDrone.constructor === createDrone}
+`);
---
+++ js-tarihcesi/functions/Sample010.js
@@ -0,0 +1,20 @@
+const _pipe = (a, b) => (arg) => b(a(arg));
+const pipe = (...ops) => ops.reduce(_pipe);
+
+const calcTotalWithTax = (pizzaCost) => pizzaCost * 1.13;
+const costForTwo = (itemCost) => Math.round((itemCost / 2) * 100) / 100;
+const cadToUSD = (cad) => Math.round(cad * 0.753653 * 100) / 100;
+const trace = (label) => (value) => {
+  console.log(`${label}: ${value}`);
+  return value;
+};
+
+const costPerPersonUsd = pipe(
+  calcTotalWithTax,
+  trace('calcTotalWithTax g'),
+  costForTwo,
+  trace('costForTwo f'),
+  cadToUSD,
+  trace('cadToUSD c')
+);
+console.log(`You have to pay $ ${costPerPersonUsd(5)} US.`); // You have to pay $2.13 in usd
