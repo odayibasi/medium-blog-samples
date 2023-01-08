0f3eebbe4fe8cdf75e4ec909e2d295567286d39c
odayibasi <odayibasi@gmail.com>
2023-01-08 20:20:48 +0300

    Updated

--- react-data/redux-toolkit/src/index.js
+++ react-data/redux-toolkit/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
+
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
